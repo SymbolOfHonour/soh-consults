ac757cacc61832129a4efae90acd61fb494d0a31
SymbolOfHonour <oluyepeadetayo@gmail.com>
2026-09-15 06:23:27 +0100

    Keep private admin routes out of crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -3,12 +3,8 @@ import { getSiteUrl } from "./site-url";
 
 export default function robots(): MetadataRoute.Robots {
   const siteUrl = getSiteUrl();
-
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: { userAgent: "*", allow: "/", disallow: ["/admin/", "/api/admin/", "/api/cron/"] },
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
