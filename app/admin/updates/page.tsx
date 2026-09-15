ba1a14c0f4bffe6ab52a439d2aedfaedf6d654c6
SymbolOfHonour <oluyepeadetayo@gmail.com>
2026-09-15 07:34:14 +0100

    Enable dashboard update search

--- app/admin/updates/page.tsx
+++ app/admin/updates/page.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import AdminUpdatesClient from "./AdminUpdatesClient";
+import AdminUpdateSearch from "./AdminUpdateSearch";
 
 export const metadata: Metadata = { title: "Update Approval Queue", robots: { index: false, follow: false } };
 
 export default function AdminUpdatesPage() {
-  return <AdminUpdatesClient />;
+  return <><AdminUpdatesClient /><AdminUpdateSearch /></>;
 }
-
